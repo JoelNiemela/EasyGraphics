55f546b445a8755685408f117d8c563af735d1af
JoelNiemela <joel@niemela.se>
2022-07-05 15:59:24 -0500

    Setup CMake

---
+++ demo/CMakeFiles/Demo.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/vendor/stb/stb.c" "CMakeFiles/Demo.dir/vendor/stb/stb.c.o" "gcc" "CMakeFiles/Demo.dir/vendor/stb/stb.c.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/src/demo_app.cpp" "CMakeFiles/Demo.dir/src/demo_app.cpp.o" "gcc" "CMakeFiles/Demo.dir/src/demo_app.cpp.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/src/main.cpp" "CMakeFiles/Demo.dir/src/main.cpp.o" "gcc" "CMakeFiles/Demo.dir/src/main.cpp.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/src/views/demo_view.cpp" "CMakeFiles/Demo.dir/src/views/demo_view.cpp.o" "gcc" "CMakeFiles/Demo.dir/src/views/demo_view.cpp.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/src/views/menu_view.cpp" "CMakeFiles/Demo.dir/src/views/menu_view.cpp.o" "gcc" "CMakeFiles/Demo.dir/src/views/menu_view.cpp.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/src/views/texture_view.cpp" "CMakeFiles/Demo.dir/src/views/texture_view.cpp.o" "gcc" "CMakeFiles/Demo.dir/src/views/texture_view.cpp.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/vendor/imgui/imgui.cpp" "CMakeFiles/Demo.dir/vendor/imgui/imgui.cpp.o" "gcc" "CMakeFiles/Demo.dir/vendor/imgui/imgui.cpp.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/vendor/imgui/imgui_demo.cpp" "CMakeFiles/Demo.dir/vendor/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/Demo.dir/vendor/imgui/imgui_demo.cpp.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/vendor/imgui/imgui_draw.cpp" "CMakeFiles/Demo.dir/vendor/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/Demo.dir/vendor/imgui/imgui_draw.cpp.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/vendor/imgui/imgui_impl_glfw.cpp" "CMakeFiles/Demo.dir/vendor/imgui/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/Demo.dir/vendor/imgui/imgui_impl_glfw.cpp.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/vendor/imgui/imgui_impl_opengl3.cpp" "CMakeFiles/Demo.dir/vendor/imgui/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/Demo.dir/vendor/imgui/imgui_impl_opengl3.cpp.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/vendor/imgui/imgui_tables.cpp" "CMakeFiles/Demo.dir/vendor/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/Demo.dir/vendor/imgui/imgui_tables.cpp.o.d"
+  "/home/mocqoro/Programming/Projects/EasyGraphics/demo/vendor/imgui/imgui_widgets.cpp" "CMakeFiles/Demo.dir/vendor/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/Demo.dir/vendor/imgui/imgui_widgets.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ demo/CMakeFiles/Demo.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo.dir/src/demo_app.cpp.o"
+  "CMakeFiles/Demo.dir/src/demo_app.cpp.o.d"
+  "CMakeFiles/Demo.dir/src/main.cpp.o"
+  "CMakeFiles/Demo.dir/src/main.cpp.o.d"
+  "CMakeFiles/Demo.dir/src/views/demo_view.cpp.o"
+  "CMakeFiles/Demo.dir/src/views/demo_view.cpp.o.d"
+  "CMakeFiles/Demo.dir/src/views/menu_view.cpp.o"
+  "CMakeFiles/Demo.dir/src/views/menu_view.cpp.o.d"
+  "CMakeFiles/Demo.dir/src/views/texture_view.cpp.o"
+  "CMakeFiles/Demo.dir/src/views/texture_view.cpp.o.d"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui.cpp.o"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui.cpp.o.d"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/Demo.dir/vendor/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/Demo.dir/vendor/stb/stb.c.o"
+  "CMakeFiles/Demo.dir/vendor/stb/stb.c.o.d"
+  "Demo"
+  "Demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
